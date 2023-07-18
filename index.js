740e9fdbf9537a4740d79e6d2df4d7d3079d043f
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-07-18 15:50:35 +0300

    react eğitimlerine başlandı

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// console.log("hello node");
+// import slugify from "slugify";
+//  const title =slugify("furkan kılıç zamanında","+")
+// console.log(title);
+
+import {hello,topla,cikar,text,user,users}from "./my-module.js";
+
+console.log(topla(2,4));
+console.log(cikar(6,4));
+console.log(text);
+console.log(user);
+console.log(users);
+hello('furkan');
---
+++ my-module.js
@@ -0,0 +1,34 @@
+// function topla(a,b){
+//     return a+b;
+// }
+
+
+// const topla=function topla(a,b){
+//     return a+b;
+// }
+
+
+// const topla=(a,b)=>{
+//     return a+b;
+// }
+
+export const hello =(name)=>{
+    // console.log("hello" +name);
+    console.log(`hello ${name}`);
+}
+
+export const text="Text";
+export const user={
+    name:"Furkan",
+    surname:"Kılıç",
+}
+
+export const users=[{name:"Furkan",
+surname:"Kılıç"},{name:"Furkan",
+surname:"Kılıç"}]
+
+
+export const topla=(a,b)=>a+b;
+export const cikar=(a,b)=>a-b;
+
+// export default topla;
