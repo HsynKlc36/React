d8a634e1b120c98ca171cde984487973ac4ca030
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-07-31 18:25:18 +0300

    useEffect tamamlandı!

---
+++ react_useeffect/src/components/Counter.js
@@ -0,0 +1,38 @@
+import {useState,useEffect} from 'react';
+
+function Counter(){
+    const [number,setNumber]=useState(0);
+    // const [name,setName]=useState('hsyn');
+  
+  useEffect(()=>{
+    console.log('Component mount edildi');//componentin doğma anıdır mount edilmesi!
+
+   const interval= setInterval(()=>{
+        setNumber((n)=>n+1);
+    },1000)
+    return ()=>clearInterval(interval);//bunun sayesinde unmount işlemini yaparız yani return  ile yapmak istediğiniz işlemi belirterek componenti sonlandırabiliriz!
+  },[])
+  
+  useEffect(()=>{
+    console.log('number state güncellendi');
+  },[number])
+  
+//   useEffect(()=>{
+//     console.log('name state güncellendi');
+//   },[name])
+
+  // useEffect(()=>{
+  //   console.log('number veya name state güncellendi');
+  // },[name,number])
+  
+    return (
+      <div className="App">
+       <h1>{number}</h1>
+  <button onClick={()=>setNumber(number+1)}>Click</button>
+  <hr/>  
+  {/* <h1>{name}</h1>  
+  <button onClick={()=>setName('hüseyin')}>Click</button>     */}
+      </div>
+    );
+}
+export default Counter;
