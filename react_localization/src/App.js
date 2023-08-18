f0a3f2e4ea315e71b716c5390f66178a99bb0d00
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-08-18 10:39:57 +0300

    react-localization tamamlandı

---
+++ react_localization/src/App.js
@@ -0,0 +1,44 @@
+import {useState,useEffect} from 'react';
+import './App.css';
+import {IntlProvider,FormattedMessage,FormattedNumber} from 'react-intl';// npm add react-intl diyerek önce paketi kurduk!
+
+const messages={
+  'tr-TR':{
+    title:'Merhaba React severler!',
+    description:'{count} yeni mesajınız var'
+  },
+  'en-US':{
+    title:'Hello, They love React!',
+    description:' you have {count} new messages'
+  }
+}
+
+function App() {
+  const defaultLocale=navigator.language // tarayıcının default dilini verir
+  console.log(defaultLocale);//tr-TR veya en-US olabilir!
+ const  [locale,setLocale]=useState(localStorage.getItem('locale')||defaultLocale)//local storage varsa oradaki değeri getirir yoksa defaultLocale'i getirecektir!
+
+ useEffect(()=>{
+  localStorage.setItem('locale',locale)
+ },[locale])
+  return (
+    <div className="App">
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <FormattedMessage id='title' />
+        <br/>
+        <br/>
+        <p>
+        <FormattedMessage id='description' values={{count:5}}/>
+        </p>
+        <button onClick={()=>setLocale('tr-TR')}>TR</button>
+        <button onClick={()=>setLocale('en-US')}>EN</button>
+        <br/>
+        <br/>
+
+      </IntlProvider>
+    
+    </div>
+  );
+}
+
+export default App;
